0bc7b17156303f070ab663352c9ec35a650f0375
Thomas Bates <tbates@ymail.com>
2017-10-22 15:21:13 -0700

    First iteration of PokePortal

---
+++ app/controllers/pokemon_controller.rb
@@ -0,0 +1,38 @@
+class PokemonController < ApplicationController
+	def capture
+		pokemon = Pokemon.find(params[:id])
+		pokemon.trainer_id = current_trainer.id
+		pokemon.save
+		redirect_to root_path
+	end
+	def damage
+		pokemon = Pokemon.find(params[:id])
+		pokemon.health -= 10
+		trainer_id = pokemon.trainer_id
+		if pokemon.health <= 0
+			pokemon.destroy
+		else
+			pokemon.save
+		end
+		redirect_to trainer_path(trainer_id)
+	end
+	def new
+		@pokemon = Pokemon.new()	
+	end		
+	def create
+		@pokemon = Pokemon.new(health:100,level:1, trainer_id: current_trainer.id)
+		@pokemon.update(new_pokemon)
+		if @pokemon.save
+			redirect_to current_trainer
+		else
+			render 'new'
+		end
+
+	end
+
+	private
+	def new_pokemon
+		params.require(:pokemon).permit(:name)
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :trainers
   root to: "home#index"
   resources :trainers
+  patch 'capture', to: "pokemon#capture", as: 'capture'
+  patch 'damage', to: 'pokemon#damage', as: 'damage'
+  get 'pokemon/new', to: 'pokemon#new', as: 'new_pokemon'
+  post 'pokemons', to: 'pokemon#create'
 end
